f1a4685452cdfcfdb36a01ba88194f43e1bbe9f3
mann <jadhavmann.mj@gmail.com>
2024-07-26 10:25:46 +0530

    update till button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('applicationForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    document.getElementById('topLeftButton').addEventListener('click', function() {
-        alert('Button clicked!');
+    document.getElementById('Menu').addEventListener('click', redirectMenu() {
+    
     });
     
     const name = document.getElementById('name').value;
@@ -12,7 +12,7 @@ document.getElementById('applicationForm').addEventListener('submit', function(e
     const address = document.getElementById('address').value;
     const course = document.getElementById('course').value;
     const form = document.getElementById('applicationForm');
-    const 
+    
 
     if (name && email && dob && phone && address && course) {
         alert('Application submitted successfully!');
